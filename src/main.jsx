2eb365cd0af65b9711b900bdf87464d39ae178fe
Usuario <usuario@gmail.com>
2024-09-13 18:52:54 -0500

    Proyecto aerolinea

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './App.css'
 import './index.css'
-import './css/navbar.css'
-import './css/modal.css'
-import './css/carrusel.css'
-import './css/radio.css'
-import './css/accordion.css'
-import './css/resumen.css'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 createRoot(document.getElementById('root')).render(
