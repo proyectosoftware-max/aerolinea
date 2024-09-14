44ab42bc3fd4b3e4d9865bc561fc5cfa9a81a341
Usuario <usuario@gmail.com>
2024-09-14 10:08:58 -0500

    Proyecto aerolinea

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react()],   
+
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',   
+
+    css: {
+      preprocessorOptions: {
+        css: {
+          additionalData: `@import "./src/index.css";`,
+        },
+      },
+    },
+  },
+});
